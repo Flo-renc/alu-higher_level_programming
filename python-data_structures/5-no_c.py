aa69716d9ab2153ebe31542cbb20bdad524621fe
Flo-renc <f.kabeya@alustudent.com>
2024-02-29 07:29:53 -0800

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
     """Remove all occurrences of 'c' and 'C' from the string."""
     result = ""
     for char in my_string:
-        if char.lower() != 'c' and char.upper != 'C' :
+        if char.lower() != 'c' and char.upper != 'C':
             result += char
     return result
