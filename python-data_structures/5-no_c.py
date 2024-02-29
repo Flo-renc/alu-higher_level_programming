4d93e00812f6489d938884c63132d99838f0cc7d
Flo-renc <f.kabeya@alustudent.com>
2024-02-29 07:21:22 -0800

    tasks completed

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    """Remove all occurrences of 'c' and 'C' from the string."""
+    result = ""
+    for char in my_string:
+        if char.lower() != 'c' and char.upper != 'C' :
+            result += char
+    return result
---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence:  # Check if the sentence is not empty
+        return len(sentence), sentence[0]
+    else:
+        return 0, None
+
+# Uncomment the following lines to test the function
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
