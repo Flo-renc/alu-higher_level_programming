d559b2c7353643da69933a173a01547a37d08d45
Flo-renc <f.kabeya@alustudent.com>
2024-03-14 00:11:52 +0200

    task 1 size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" calling class square """
+
 class Square:
+
     ''' making size a private attibute by adding 2 __ before it e.g __.size'''
+
     def __init__(self, size):
+        """ an method for size """
         self.__size = size
