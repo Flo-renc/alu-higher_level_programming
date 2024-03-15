d2cc364a0b8e0dec037606f238f0e6a67b74f45d
Flo-renc <f.kabeya@alustudent.com>
2024-03-15 19:46:29 +0200

    task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """class module"""
+
+
 class Rectangle:
     """Class Rectangle public class atribute number_of_instances"""
     number_of_instances = 0
-
-
     def __init__(self, width=0, height=0):
         """Initialization method"""
         self.width = width
