185e8ced31bd2e8a17f3fd4d2cb33ef849c8cdb3
Flo-renc <f.kabeya@alustudent.com>
2024-03-16 00:06:36 +0200

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" class module """
+
+
+def inherits_from(obj, a_class):
+    """Check if the object is an instance of a class that inherited from the specified class."""
+    return issubclass(type(obj), a_class) and type(obj) != a_class 
