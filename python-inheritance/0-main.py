6d69078993bc3f7b970e28ab3de3adba66c68dd1
Flo-renc <f.kabeya@alustudent.com>
2024-03-15 21:46:26 +0200

    task 0

--- python-inheritance/0-main.py
+++ python-inheritance/0-main.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 lookup = __import__('0-lookup').lookup
+"""class module """
+
 
 class MyClass1(object):
+    """class Myclass1"""
     pass
+"""class module """
+
 
 class MyClass2(object):
+    """ class Myclass """
     my_attr1 = 3
+
     def my_meth(self):
+        """ method """
         pass
 
 print(lookup(MyClass1))
