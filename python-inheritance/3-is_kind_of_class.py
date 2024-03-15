747c432e54597d18cc02a0b5379be9e67a65300f
Flo-renc <f.kabeya@alustudent.com>
2024-03-15 23:48:04 +0200

    task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,5 +4,4 @@
 
 def is_kind_of_class(obj, a_class):
     """ to check if an instance is an object of a superclass and a subclass """
-    return isinstance(obj, a_class)
-#he function first checks if the object is an instance of the specified class using isinstance(obj, a_class), which accounts for inheritance. 
+    return isinstance(obj, a_class) 
