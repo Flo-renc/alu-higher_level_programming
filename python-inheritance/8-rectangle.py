afa2c4988f7093548010dfcfd7077f639d4fa59f
Flo-renc <f.kabeya@alustudent.com>
2024-03-16 01:37:13 +0200

    tasks

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,37 @@
+
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class BaseGeometry:
+    """ class """
+    def area(self):
+        """ to rasie an exception """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(name + ' must be an integer')
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
+        self.name = name
+        self.value = value
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle, inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """ constructor method """
+        super().__init__()
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
