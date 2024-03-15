379562a5fca173eaf4b818b27de2a7b7dff160fa
Flo-renc <f.kabeya@alustudent.com>
2024-03-15 21:50:04 +0200

    task 1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" defines an object attribut lookup function """
+
+
 def lookup(obj):
     """Returns a list of available attributes and methods of an object"""
     return dir(obj)
