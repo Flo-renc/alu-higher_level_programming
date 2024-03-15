e8403b966c85ab2dcf2002d39bc9eb532ac3b026
Flo-renc <f.kabeya@alustudent.com>
2024-03-16 00:27:39 +0200

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class module """
+
+
+class BaseGeometry:
+    """ class """
+    def area(self):
+        """ to rasie an exception """
+        raise Exception('area() is not implemented')
