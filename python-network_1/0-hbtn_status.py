e8ce2154fbf02cc46a54dce641e544b9499fca96
Flo-renc <f.kabeya@alustudent.com>
2024-03-24 10:22:19 +0200

    task 4

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,13 +3,36 @@
 
 
 import urllib.request
+def fetch_url(url):
+    """
+    Fetches the content of a URL using urllib.
 
-url = 'https://alu-intranet.hbtn.io/status'
+    Args:
+        url (str): The URL to fetch.
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
+    Returns:
+        bytes: The content of the response.
 
-print("Body response:")
-print("\t- type:", type(content))
-print("\t- content:", content)
-print("\t- utf8 content:", content.decode('utf-8'))
+    Raises:
+        URLError: If there was an error fetching the URL.
+    """
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+    return content
+
+def display_response_body(content):
+    """
+    Displays the body of the response.
+
+    Args:
+        content (bytes): The content of the response.
+    """
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode('utf-8'))
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5050/status'
+    response_content = fetch_url(url)
+    display_response_body(response_content)
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""Fetches URL status and displays response"""
+"""
+Module to fetch the status of a URL and display the response body.
+"""
+
 import requests
 
-
 def fetch_status(url):
-  """Fetches the status of a URL and displays the response body.
+    """Fetches the status of a URL and displays the response body.
 
-  Args:
-      url: The URL to fetch the status from.
-  """
-  try:
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for non-2xx status codes
-    print(f"Body response:\n{response.text}")
-  except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
+    Args:
+        url (str): The URL to fetch the status from.
+
+    Raises:
+        RequestException: If an error occurs while fetching the URL.
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for non-2xx status codes
+        print(f"Body response:\n{response.text}")
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-  url = "https://alu-intranet.hbtn.io/status"
-  fetch_status(url)
+    url = "https://alu-intranet.hbtn.io/status"
+    fetch_status(url)
