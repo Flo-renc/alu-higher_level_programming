c9c1240d017b3fbea97451a48845efcd19528a84
Flo-renc <f.kabeya@alustudent.com>
2024-03-24 10:27:28 +0200

    task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,26 +1,10 @@
 #!/usr/bin/python3
-"""
-Module to fetch the status of a URL and display the response body.
-"""
-
+"""Fetches URL status and displays response"""
 import requests
-
-def fetch_status(url):
-    """Fetches the status of a URL and displays the response body.
-
-    Args:
-        url (str): The URL to fetch the status from.
-
-    Raises:
-        RequestException: If an error occurs while fetching the URL.
-    """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for non-2xx status codes
-        print(f"Body response:\n{response.text}")
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
-    fetch_status(url)
+    resp = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))
