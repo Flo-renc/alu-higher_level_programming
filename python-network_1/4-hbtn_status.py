40a846ceed0dbe1251dd4f277f53827475554da9
Flo-renc <f.kabeya@alustudent.com>
2024-03-24 10:45:35 +0200

    task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,3 +8,10 @@ if __name__ == "__main__":
     print("Body response:")
     print("\t- type: {}".format(type(resp.text)))
     print("\t- content: {}".format(resp.text))
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    resp = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))
