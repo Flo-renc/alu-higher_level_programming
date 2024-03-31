4a64b2835e7bfb7a4d88de61eb8c8139ee666837
Flo-renc <f.kabeya@alustudent.com>
2024-03-31 11:17:06 +0200

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,18 +1,49 @@
-#!/usr/bin/python3
-""" defines a function that prints a text with 2 new lines """
-
-
 def text_indentation(text):
-    """ a function that prints a text with 2 new lines after
-    each of these characters: ., ? and :
+    """Prints text with 2 new lines after each '.', '?', and ':' character.
 
     Args:
-        text must be a string
+        text (str): The input text.
 
     Raises:
-        TypeError: if text is not string. error message text must be a string
+        TypeError: If text is not a string.
 
+    Example:
+        >>> text_indentation("Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres")
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    
+    Quonam modo?
+    
+    Utrum igitur tibi litteram videor an totas paginas commovere?
+    
+    Non autem hoc:
+    
+    igitur ne illud quidem.
+    
+    Fortasse id optimum, sed ubi illud:
+    
+    Plus semper voluptatis?
+    
+    Teneo, inquit, finem illi videri nihil dolere.
+    
+    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum rationi oboediens.
+    
+    Si id dicis, vicimus.
+    
+    Inde sermone vario sex illa a Dipylo stadia confecimus.
+    
+    Sin aliud quid voles, postea.
+    
+    Quae animi affectio suum cuique tribuens atque hanc, quam dico.
     """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
@@ -29,3 +60,16 @@ def text_indentation(text):
 
     for line in lines:
         print(line)
+        print()
+        print()
+
+if __name__ == "__main__":
+    text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
