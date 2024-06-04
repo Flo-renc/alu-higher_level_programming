ccc49f04bb0e82262bc4531a79e87a42f4f1b905
Flo-renc <f.kabeya@alustudent.com>
2024-06-04 01:04:02 -0700

    3 languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let firstLang = 'C is fun';
+let secondLang = 'Python is cool';
+let thirdLang = 'Javascript is amazing';
+console.log(firstLang);
+console.log(secondLang);
+console.log(thirdLang);
