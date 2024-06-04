29682e3a80bb3825063ce60822411bffc05317d6
Flo-renc <f.kabeya@alustudent.com>
2024-06-04 04:13:53 -0700

    Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Check the number of arguments passed
+const numArgs = process.argv.length - 2; // Subtract 2 to exclude "node" and script name
+
+// Print message based on the number of arguments
+if (numArgs === 0) {
+    console.log('No argument');
+} else if (numArgs === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
